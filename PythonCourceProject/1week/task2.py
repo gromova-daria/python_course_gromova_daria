b022c981c68cfe5f23db36bb10226f2bbb049db6
gromova-daria <146058791+gromova-daria@users.noreply.github.com>
2024-09-22 14:09:54 +0300

    Create task2.py

---
+++ PythonCourceProject/1week/task2.py
@@ -0,0 +1,29 @@
+"""
+leetcode.com/problem-list/string/
+url: https://leetcode.com/problems/longest-palindromic-substring/?envType=problem-list-v2&envId=string
+"""
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if not s:
+            return ""
+
+        start, end = 0, 0
+
+        for i in range(len(s)):
+            len1 = self.expand_around_center(s, i, i)
+            len2 = self.expand_around_center(s, i, i + 1)
+            max_len = max(len1, len2)
+
+            if max_len > end - start:
+                start = i - (max_len - 1) // 2
+                end = i + max_len // 2
+
+        return s[start:end + 1]
+
+    def expand_around_center(self, s: str, left: int, right: int) -> int:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 1
